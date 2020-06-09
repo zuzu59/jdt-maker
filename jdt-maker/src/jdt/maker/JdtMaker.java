6d9c38cb232f86e3c2ee729bca97dd28c923ba2b
Christian Zufferey <christian@zufferey.com>
2020-06-09 15:20:46 +0200

    [DEMO] Using the NetBeans form editor - the design and the management of events were created with the Netbeans form editor. This allows you to be more efficient when you want to modify the behavior of certain elements or add more afterwards.

--- jdt-maker/src/jdt/maker/JdtMaker.java
+++ jdt-maker/src/jdt/maker/JdtMaker.java
@@ -3,7 +3,8 @@ package jdt.maker;
 public class JdtMaker {
 
     public static void main(String[] args) {
-        Window win1 = new Window();
+        //Window_z1 win1 = new Window_z1();
+        new Window_zf2().setVisible(true);
     }
 
 }
